ec5e1519b157218e1afb0a02ce3ff8677f9b6b79
Vuongnd22 <nguyenvuong8304@gmail.com>
2025-02-21 11:30:18 +0700

    SG6L-6

--- src/java/dal/BillDAO.java
+++ src/java/dal/BillDAO.java
@@ -1,34 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dal;
-
 import model.Bill;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author ADMIN
  */
 public class BillDAO extends DBContext {
-
+    private static final Logger LOGGER = Logger.getLogger(BillDAO.class.getName());
     public void inserToBill(Bill bill) {
         String sql = """
                      INSERT INTO Bill (TableID, DishID, Quantity, Price)
                      VALUES 
                          (?, ?, ?, ?)
                      """;
-        try {
-            PreparedStatement st = connection.prepareStatement(sql);
+         try (PreparedStatement st = connection.prepareStatement(sql)) {
             st.setInt(1, bill.getTableId());
             st.setInt(2, bill.getDishId());
             st.setInt(3, bill.getQuantity());
             st.setInt(4, bill.getPrice());
             st.executeUpdate();
         } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Error inserting bill into database", e);
         }
     }
 }
