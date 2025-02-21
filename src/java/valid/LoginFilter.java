ec5c840f8d70747cd2cdef76ff73cd6dcfc4df13
B-Kha <Khabnhe182308@fpt.edu.vn>
2025-02-21 13:35:18 +0700

    Update LoginFilter.java
    Remove unused method

--- src/java/valid/LoginFilter.java
+++ src/java/valid/LoginFilter.java
@@ -24,7 +24,7 @@ import jakarta.servlet.http.HttpSession;
  */
 public class LoginFilter implements Filter {
 
-    private static final boolean debug = true;
+    private static final boolean DEBUG = true;
 
     // The filter configuration object we are associated with.  If
     // this value is null, this filter instance is not currently
@@ -34,33 +34,7 @@ public class LoginFilter implements Filter {
     public LoginFilter() {
     }
 
-    private void doBeforeProcessing(ServletRequest request, ServletResponse response)
-            throws IOException, ServletException {
-        if (debug) {
-            log("LoginFilter:DoBeforeProcessing");
-        }
-
-        // Write code here to process the request and/or response before
-        // the rest of the filter chain is invoked.
-        // For example, a logging filter might log items on the request object,
-        // such as the parameters.
-        /*
-	for (Enumeration en = request.getParameterNames(); en.hasMoreElements(); ) {
-	    String name = (String)en.nextElement();
-	    String values[] = request.getParameterValues(name);
-	    int n = values.length;
-	    StringBuffer buf = new StringBuffer();
-	    buf.append(name);
-	    buf.append("=");
-	    for(int i=0; i < n; i++) {
-	        buf.append(values[i]);
-	        if (i < n-1)
-	            buf.append(",");
-	    }
-	    log(buf.toString());
-	}
-         */
-    }
+   
 
     private void doAfterProcessing(ServletRequest request, ServletResponse response)
             throws IOException, ServletException {
